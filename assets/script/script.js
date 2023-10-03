2cbf647f55438232a0b2405f70bdae619882ac0e
DillenGrant <dillen.grant72@gmail.com>
2023-10-02 19:36:04 -0600

    modal classes

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,10 +1,16 @@
 const modal = document.querySelector('.modal')
 const getMovie = document.querySelector('.get-movie')
 const close = document.querySelector('.close')
+const store = document.querySelector('.store')
 
 getMovie.addEventListener('click', () => {
     modal.show();
 })
+
 close.addEventListener('click', () => {
     modal.close();
+})
+
+store.addEventListener('click', () => {
+    
 })
