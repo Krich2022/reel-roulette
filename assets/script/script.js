1970f3ceca01adcf1989de4d2671169e00785254
Oddux <141483016+Oddux@users.noreply.github.com>
2023-10-05 02:56:36 -0600

    Styling and working buttons

--- assets/script/script.js
+++ assets/script/script.js
@@ -13,3 +13,9 @@
   document.getElementById('closeModal').addEventListener('click', function () {
     document.getElementById('modal').classList.add('hidden');
   });
+
+  $(document).ready(function () {
+    $(".toggleList").click(function () {
+      $(this).siblings("#checkboxList").toggleClass("hidden");
+    });
+  });
